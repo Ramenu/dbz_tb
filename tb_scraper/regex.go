0d1f10a1d75ab2c01270b590a7c573fb2a6ecb10
ramenu <ramenu101@gmail.com>
2022-09-19 17:41:57 -0400

    add getRarityReg fn

--- tb_scraper/regex.go
+++ tb_scraper/regex.go
@@ -6,3 +6,8 @@ func GetNameReg() *regexp.Regexp {
 	const nameReg = "<td><a href=\".*\" title=\".*\">(.*)</a>"
 	return regexp.MustCompile(nameReg)
 }
+
+func getRarityReg() *regexp.Regexp {
+	const categoryReg = "<a href=\"/wiki/Category:[NRSUL]?[NSR]?[NR]\" title=\"Category:([NRSUL]?[NSR]?[NR])\">"
+	return regexp.MustCompile(categoryReg)
+}
