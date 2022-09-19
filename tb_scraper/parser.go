6ea3f142fcaf3d7afe98765b5f1787ef70d06e3f
ramenu <ramenu101@gmail.com>
2022-09-19 17:16:42 -0400

    add parser.go

--- tb_scraper/parser.go
+++ tb_scraper/parser.go
@@ -1,1 +1,16 @@
 package main
+
+import "strings"
+
+// Some characters are invalid for HTML, so when
+// scraping the data it is important to fix the
+// sequences into their proper characters.
+func fixHTMLSequences(s string) string {
+	if strings.Contains(s, "&#39;") { // '
+		return strings.Replace(s, "&#39;", "'", -1)
+	} 
+	if strings.Contains(s, "&amp;") { // &
+		return strings.Replace(s, "&amp;", "&", -1)
+	}
+	return s
+}
