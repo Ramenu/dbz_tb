38310c69489278a7d7fd4a5eb6c321e899532d45
ramenu <ramenu101@gmail.com>
2022-09-20 10:57:31 -0400

    add removeHTMLTags fn

--- tb_scraper/parser.go
+++ tb_scraper/parser.go
@@ -2,9 +2,9 @@ package main
 
 import "strings"
 
-// Some characters are invalid for HTML, so when
+// Some s[i]racters are invalid for HTML, so when
 // scraping the data it is important to fix the
-// sequences into their proper characters.
+// sequences into their proper s[i]racters.
 func fixHTMLSequences(s string) string {
 	if strings.Contains(s, "&#39;") { // '
 		return strings.Replace(s, "&#39;", "'", -1)
@@ -13,4 +13,38 @@ func fixHTMLSequences(s string) string {
 		return strings.Replace(s, "&amp;", "&", -1)
 	}
 	return s
+}
+
+
+// Returns a string with the HTML
+// tags removed (Note: at the moment this 
+// only handles the href tags).
+func removeHTMLTags(s string) string {
+	length := len(s)
+	var tagStartCount uint64 = 0
+	var tagsRemovedS string
+	tagEnded := true
+	for i := range s {
+		if s[i] == '<' {
+			tagStartCount++
+			// Check if its a closing tag
+			if i + 1 < length {
+				if s[i + 1] == '/' {
+					tagStartCount -= 2 // We just incremented it, so sub by 2 is necessary
+					tagEnded = false
+				}
+			}
+		}
+
+		if !tagEnded && s[i] == '>' {
+			tagEnded = true
+			continue
+		}
+
+		if tagStartCount == 0 && tagEnded {
+			tagsRemovedS += string(s[i])
+		}
+
+	}
+	return fixHTMLSequences(tagsRemovedS)
 }
