b7130a2e10bc90862f36d5110b8748512ae0c8d0
ramenu <ramenu101@gmail.com>
2022-09-20 10:57:43 -0400

    modify/add new regex fns

--- tb_scraper/regex.go
+++ tb_scraper/regex.go
@@ -3,11 +3,20 @@ package main
 import "regexp"
 
 func GetNameReg() *regexp.Regexp {
-	const nameReg = "<td><a href=\".*\" title=\".*\">(.*)</a>"
+	const nameReg = "<td><a href=\"(.*)\" title=\".*\">(.*)</a>"
 	return regexp.MustCompile(nameReg)
 }
 
-func getRarityReg() *regexp.Regexp {
+func GetRarityReg() *regexp.Regexp {
 	const categoryReg = "<a href=\"/wiki/Category:[NRSUL]?[NSR]?[NR]\" title=\"Category:([NRSUL]?[NSR]?[NR])\">"
 	return regexp.MustCompile(categoryReg)
+}
+
+func GetSuperAtkReg() *regexp.Regexp {
+	const superAtkReg = "(?s)<tr><td colspan=\"2\">(.*?)</td></tr>"
+	return regexp.MustCompile(superAtkReg)
+}
+
+func replaceHTMLTypeIcons(s string) string {
+	return ""
 }
