7630a1513ce6789c2d66bdb6b729134c9ee8dc51
ramenu <ramenu101@gmail.com>
2022-09-20 15:57:02 -0400

    html tag removal actually works now

--- tb_scraper/parser.go
+++ tb_scraper/parser.go
@@ -2,52 +2,50 @@ package main
 
 import (
 	"strings"
+	"github.com/microcosm-cc/bluemonday"
 )
+
+
+var policy = bluemonday.StrictPolicy()
+var rePattern = GetCitationReg()
 
 // Some characters are invalid for HTML, so when
 // scraping the data it is important to fix the
 // sequences into their proper characters.
 func fixHTMLSequences(s string) string {
 	if strings.Contains(s, "&#39;") { // '
-		return strings.Replace(s, "&#39;", "'", -1)
+		s = strings.ReplaceAll(s, "&#39;", "'")
 	} 
 	if strings.Contains(s, "&amp;") { // &
-		return strings.Replace(s, "&amp;", "&", -1)
+		s = strings.ReplaceAll(s, "&amp;", "&")
+	}
+	if strings.Contains(s, "&lt;") {
+		s = strings.ReplaceAll(s, "&lt;", "<")
+	}
+	if strings.Contains(s, "&gt;") {
+		s = strings.ReplaceAll(s, "&gt;", ">")
+	}
+	if strings.Contains(s, "&le;") {
+		s = strings.ReplaceAll(s, "&le;", "<=")
+	}
+	if strings.Contains(s, "&ge;") {
+		s = strings.ReplaceAll(s, "&ge;", ">=")
+	}
+	if strings.Contains(s, "&#34;") {
+		s = strings.ReplaceAll(s, "&#34;", "\"")
 	}
 	return s
 }
 
 
-// Returns a string with the HTML
-// tags removed (Note: at the moment this 
-// only handles the href tags).
+
+// Returns a string with the HTML tags stripped
+// (also fixes HTML character junk).
 func removeHTMLTags(s string) string {
-	length := len(s)
-	var tagStartCount uint64 = 0
-	var tagsRemovedS string
-	tagEnded := true
-	for i := range s {
-		if s[i] == '<' {
-			tagStartCount++
-			// Check if its a closing tag
-			if i + 1 < length {
-				if s[i + 1] == '/' {
-					tagStartCount -= 2 // We just incremented it, so sub by 2 is necessary
-					tagEnded = false
-				}
-			}
-		}
-
-		if !tagEnded && s[i] == '>' {
-			tagEnded = true
-			continue
-		}
-
-		if tagStartCount == 0 && tagEnded {
-			tagsRemovedS += string(s[i])
-		}
-
+	s = policy.Sanitize(s)
+	if rePattern.MatchString(s) {
+		s = rePattern.ReplaceAllString(s, "")
 	}
-	return fixHTMLSequences(tagsRemovedS)
+	return fixHTMLSequences(s)
 }
 
