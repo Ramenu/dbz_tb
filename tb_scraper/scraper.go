655275a076cea5b7d94ab247cf7c517d2a1dbaf8
ramenu <ramenu101@gmail.com>
2022-09-20 10:58:06 -0400

    update scraper with card url fetching and sa fetch

--- tb_scraper/scraper.go
+++ tb_scraper/scraper.go
@@ -22,12 +22,16 @@ func getPageTemplate(responseBody *io.ReadCloser) string {
 
 // Returns true if the page actually contains anything
 // useful.
-func isValidPage(err error, responseBody string) bool {
+func isValidPage(responseBody string) bool {
 	emptyPage := strings.Contains(responseBody, "There is currently no text in this page.")
-	return !emptyPage
+	invalidUnit := strings.Contains(responseBody, "This character is unreleased, you can only fight it as a boss") ||
+	               strings.Contains(responseBody, "Wind-up Nutcracker")
+	               
+
+	return !emptyPage && !invalidUnit
 }
 
-// Updates the URL to the 'next' page of characters. 
+// Updates the URL to the 'next' page of units. 
 // Should be called only after the first URL has
 // been gone through.
 func updateURL(end *int, seriesNumber string) page {
@@ -44,6 +48,10 @@ func main() {
 
 	var allUnitNames []string
 	var allRarityNames []string
+	nameReg := GetNameReg()
+	rarityReg := GetRarityReg()
+	saReg := GetSuperAtkReg()
+	//var allSuperAttackNames []string
 	urls := [4]string{ 
 		"https://dbz-dokkanbattle.fandom.com/wiki/All_Cards:_(1)001_to_(1)100",
 		"https://dbz-dokkanbattle.fandom.com/wiki/All_Cards:_(2)001_to_(2)1000",
@@ -56,15 +64,31 @@ func main() {
 		end := 100
 		responseBody := getPageTemplate(&page.response.Body)
 
-		for isValidPage(err, responseBody){
+		for isValidPage(responseBody){
 			
-			nameMatches := GetNameReg().FindAllStringSubmatch(responseBody, -1)
-			rarityMatches := getRarityReg().FindAllStringSubmatch(responseBody, -1)
-			for i := range nameMatches {
+			urlNameMatches := nameReg.FindAllStringSubmatch(responseBody, -1)
+			rarityMatches := rarityReg.FindAllStringSubmatch(responseBody, -1)
+			for i := range urlNameMatches {
 				// To ensure only names are appended into the slice
-				if !strings.Contains(nameMatches[i][1], "img alt="){ 
-					allUnitNames = append(allUnitNames, fixHTMLSequences(nameMatches[i][1]))
+				if !strings.Contains(urlNameMatches[i][2], "img alt="){ 
+					allUnitNames = append(allUnitNames, fixHTMLSequences(urlNameMatches[i][2]))
 				} 
+
+				// Get the other info from the character's wiki link
+				if !strings.Contains(urlNameMatches[i][1], "Category"){
+					fullInfoURL := "https://dbz-dokkanbattle.fandom.com" + urlNameMatches[i][1]
+					infoResponse, infoErr := http.Get(fullInfoURL)
+					if infoErr == nil {
+						infoResponseBody := getPageTemplate(&infoResponse.Body)
+						if isValidPage(infoResponseBody){
+							//fmt.Println("Link: ", fullInfoURL)
+							unitDescription := saReg.FindAllStringSubmatch(infoResponseBody, -1)
+							saMatch := unitDescription[1][1] // This gets the super attack
+							fmt.Println("Super attack: ", removeHTMLTags(saMatch))
+						}
+					}
+
+				}
 			}
 
 			for i := range rarityMatches {
@@ -81,7 +105,7 @@ func main() {
 		fmt.Println(name)
 	}*/
 
-	for _, category := range allRarityNames {
+	/*for _, category := range allRarityNames {
 		fmt.Println(category)
-	}
+	}*/
 }
