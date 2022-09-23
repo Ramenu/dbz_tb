44c2f85a2f2341893f22de518029d6def3080e44
ramenu <ramenu101@gmail.com>
2022-09-23 11:40:31 -0400

    added regex for each component of unit desc

--- tb_scraper/regex.go
+++ tb_scraper/regex.go
@@ -5,18 +5,21 @@ import (
 	"strings"
 )
 
-
 const nameRegPattern = "<td><a href=\"(.*)\" title=\".*\">(.*)</a>"
-const rarityRegPattern = "<a href=\"/wiki/Category:[NRSUL]?[NSR]?[NR]\" title=\"Category:([NRSUL]?[NSR]?[NR])\">"
-const superAtkRegPattern = "(?s)<tr><td colspan=\"2\">(.*?)</td></tr>"
-const citationRegPattern = "\\[\\d\\]";
+const rarityRegPattern = "<a href=\"/wiki/Category:[NRSUL]?[NSR]?[NR]\" title=\"Category:([NRSUL]?[NSR]?[NR])\">" // fix this?
+const leaderSkillRegPattern = "(?s)Leader_Skill\\.png.*?<tr>.*?<td colspan=\"2\">(.*?)</td>.*?</tr>"
+const superAtkRegPattern = "(?s)Super_atk\\.png.*?<tr>.*?<td colspan=\"2\">(.*?)</td>.*?</tr>"
+const citationRegPattern = "\\[\\d\\]"
 const typeIconRegPattern = "<a href=\"\\/wiki\\/Category:\\w*_?[A-Z][A-Z][A-Z]\" title=\"Category:(\\w*? ?[A-Z][A-Z][A-Z])\">"
+const passiveRegPattern = "(?s)Passive_skill\\.png.*?<tr>.*?<td colspan=\"2\">(.*?)</td>.*?</tr>"
 
 var nameReg = regexp.MustCompile(nameRegPattern)
 var rarityReg = regexp.MustCompile(rarityRegPattern)
+var leaderSkillReg = regexp.MustCompile(leaderSkillRegPattern)
 var superAtkReg = regexp.MustCompile(superAtkRegPattern)
 var citationReg = regexp.MustCompile(citationRegPattern)
 var typeIconReg = regexp.MustCompile(typeIconRegPattern)
+var passiveReg = regexp.MustCompile(passiveRegPattern)
 
 func GetNameReg() *regexp.Regexp {
 	return nameReg
@@ -26,12 +29,20 @@ func GetRarityReg() *regexp.Regexp {
 	return rarityReg
 }
 
+func GetLeaderSkillReg() *regexp.Regexp {
+	return leaderSkillReg
+}
+
 func GetSuperAtkReg() *regexp.Regexp {
 	return superAtkReg
 }
 
 func GetCitationReg() *regexp.Regexp {
 	return citationReg
+}
+
+func GetPassiveReg() *regexp.Regexp {
+	return passiveReg
 }
 
 // Replaces the HTML type icons with the just the
