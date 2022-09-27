342af92ccf000dbcda01408df5d73bce666f8e8c
ramenu <ramenu101@gmail.com>
2022-09-27 12:57:11 -0400

    add more keywords

--- tb_parser/src/main.rs
+++ tb_parser/src/main.rs
@@ -3,7 +3,7 @@ fn main()
 {
     let mut s = String::from("ATK and DEF +120% at start of turn; Ki +2 in addition per  Ki Sphere obtained; perform Potara fusion when conditions are met");
     while tokenizer::has_more_tokens(&s) {
-        let token = tokenizer::get_next_token(&mut s);
-        println!("{}", token.expect("Failed to retrieve match").0);
+        let token = tokenizer::get_next_token(&mut s, true).expect("No tokens remaining.");
+        println!("{}, token is: {}", token.0, token.1);
     }
 }
--- tb_parser/src/tokenizer.rs
+++ tb_parser/src/tokenizer.rs
@@ -2,13 +2,30 @@
 
 use regex::Regex;
 use lazy_static::lazy_static;
+use std::fmt;
+
 pub enum Token 
 {
-    BinOp,
+    Op,
     Identifier,
     Keyword,
     Number,
     Null
+}
+
+#[cfg(debug_assertions)]
+impl fmt::Display for Token 
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match self 
+        {
+            Token::Op => "Binary Operator",
+            Token::Keyword => "Keyword",
+            Token::Number => "Number",
+            _ => "Identifier"
+        };
+        write!(f, "{}", s)
+    }
 }
 
 #[inline]
@@ -48,13 +65,51 @@ pub fn get_binary_op(s : &str) -> Option<char>
 
 pub fn get_token(s : &str) -> Token 
 {
-    let token: Token = match s 
+    let token: Token = match s.to_lowercase().as_str()
     {
-        "Raises" | "ATK" | "DEF" | "HP" | "Category" |
-        "Type" | "and" | "or" | "STR" | "AGL" | "TEQ" |
-        "PHY" | "INT" | "Ki" => return Token::Keyword,
+        "raises" | "atk" | "def" | "hp" | "category" |
+        "type" | "and" | "or" | "str" | "agl" | "teq" |
+        "phy" | "int" | "ki" | "start" | "end" | "of" |
+        "turn" | "in" | "addition" | "per" | "conditions" |
+        "are" | "met" | "when" | "below" | "is" | "causes" |
+        "to" | "deliver" | "the" | "recover" | "meter" | "more" |
+        "as" | "for" | "once" | "only" | "rare" | "chance" |
+        "stun" | "seal" | "all" | "allies" | "super" | "attacks" |
+        "attack" | "performing" | "a" | "may" | "being" | "1st" |
+        "attacker" | "enemy's" | "allies'" | "facing" | "enemies'" |
+        "dealt" | "reduce" | "reduces" | "with" | "enemy" | 
+        "above" | "damage" | "low" | "high" | "very" | "first" |
+        "stunning" | "sealing" | "by" | "reducing" | "that" |
+        "lowers" | "increases" | "increase" | "decreases" | 
+        "decrease" | "guard" | "received" | "activated" |
+        "activate" | "increasing" | "decreasing" | "disable" |
+        "enable" | "reduced" | "active" | "immune" | "negative" |
+        "effects" | "effect" | "final" | "blow" | "next" | "2nd" |
+        "3rd" | "second" | "third" | "enemies" | "greatly" | 
+        "briefly" | "upon" | "attacked" | "delivered" | 
+        "temporarily" | "row" | "defense" | "massively" | "each" |
+        "up" | "plus" | "an" | "additional" | "sphere" | "obtained" |
+        "medium" | "evade" | "evading" | "greater" | "stats" | 
+        "boost" | "starting" | "from" | "battle" | "counter" |
+        "countering" | "tremendous" | "power" | "class" | "including" |
+        "effective" | "against" | "target" | "ultimate" | "within" | 
+        "same" | "after" | "launching" | "character" | "receives" | 
+        "revives" | "ally" | "attacking" | "KO'd" | "KO" |
+        "character's" | "recovered" | "great" | "huge" | "times" |
+        "absorbs" | "can" | "obtain" | "whose" | "name" | "includes" |
+        "less" | "transforms" | "entering" | "transformation" | "bigger" |
+        "smaller" | "becoming" | "team" | "stunned" | "sealed" | "foresees" |
+        "foresee" | "whenever" | "fuses" | "sneezes" | "switches" |
+        "awakens" | "existing" | "exchange" | "randomly" | "using" | 
+        "guaranteed" | "hit" | "into" | "rages" | "time" | "rises" |
+        "turns" | "exchanges" | "another" | "through"| "if" | "deactivates" |
+        "other" | "than" | "excluded" | "included" | "certain" | "entrance" |
+        "animation" | "guards" | "critical" | "not" | "single" | "targeted" |
+        "their" | "every" | "perform" | "rest" | "it" | "ultra" | "belong" |
+        "etc" | "at" => return Token::Keyword,
 
-        "+" | "-" | "*" | "/" | ";" | "&" => return Token::BinOp,
+        // Note '/' can be used for OR options like 'Enemies/Allies' ATK +10%' 
+        "+" | "-" | "*" | "/" | ";" | "&" | ">" | "=" | "<" | "\"" | "%" => return Token::Op,
         _ => Token::Identifier
     };
 
@@ -70,17 +125,19 @@ pub fn has_more_tokens(s : &str) -> bool {
     return !s.trim().is_empty();
 }
 
-pub fn get_next_token(s : &mut String) -> Option<(String, Token)>
+pub fn get_next_token(s : &mut String, advance : bool) -> Option<(String, Token)>
 {
     lazy_static! {
-        static ref RE : Regex = Regex::new(r"([^\s\W]*[^\W]|[^\w\s])").expect("Failed to compile regex"); //  WHEN U WAKE UP: this works
+        static ref RE : Regex = Regex::new(r"([^\s\W]*[\.']?[^\W][\.']?|[^\w\s])").expect("Failed to compile regex"); 
     }
 
     if RE.is_match(s) 
     {
         let found = RE.find(&s).expect("Unable to find match in string").as_str().to_string();
         let token = get_token(&found);
-        *s = RE.replace(s, "").to_string();
+        if advance {
+            *s = RE.replace(s, "").to_string();
+        }
         return Some((found, token));
     }
     return None;
