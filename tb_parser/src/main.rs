6d1236910bb050f0b8e67111759756919fb3fafb
ramenu <ramenu101@gmail.com>
2022-09-27 10:49:42 -0400

    fix get_next_token

--- tb_parser/src/main.rs
+++ tb_parser/src/main.rs
@@ -3,6 +3,7 @@ fn main()
 {
     let mut s = String::from("ATK and DEF +120% at start of turn; Ki +2 in addition per  Ki Sphere obtained; perform Potara fusion when conditions are met");
     while tokenizer::has_more_tokens(&s) {
-        tokenizer::get_next_token(&mut s);
+        let token = tokenizer::get_next_token(&mut s);
+        println!("{}", token.expect("Failed to retrieve match").0);
     }
 }
--- tb_parser/src/tokenizer.rs
+++ tb_parser/src/tokenizer.rs
@@ -1,11 +1,14 @@
 
+
 use regex::Regex;
 use lazy_static::lazy_static;
-pub enum Token {
+pub enum Token 
+{
     BinOp,
     Identifier,
     Keyword,
-    Number
+    Number,
+    Null
 }
 
 #[inline]
@@ -13,13 +16,45 @@ pub fn is_number(s : &String) -> bool {
     return s.parse::<i32>().is_ok();
 }
 
-pub fn get_token(s : &str) -> Token {
-    let token: Token = match s {
+#[inline]
+pub fn s_is_binary_op(s : &str) -> bool 
+{
+    return match s {
+        "+" | "-" | "*" | "/" | ";" | "&" => true,
+        _ => false
+    };
+}
+
+#[inline]
+pub fn c_is_binary_op(c : char) -> bool {
+    return match c 
+    {
+        '+' | '-' | '*' | '/' | ';' | '&' => true,
+        _ => false
+    };
+}
+
+pub fn get_binary_op(s : &str) -> Option<char> 
+{
+    for (_, c) in s.chars().enumerate() 
+    {
+        if c_is_binary_op(c) {
+            return Some(c);
+        }
+    }
+    return None;
+}
+
+
+pub fn get_token(s : &str) -> Token 
+{
+    let token: Token = match s 
+    {
         "Raises" | "ATK" | "DEF" | "HP" | "Category" |
         "Type" | "and" | "or" | "STR" | "AGL" | "TEQ" |
         "PHY" | "INT" | "Ki" => return Token::Keyword,
 
-        "+" | "-" | "*" | "/" => return Token::BinOp,
+        "+" | "-" | "*" | "/" | ";" | "&" => return Token::BinOp,
         _ => Token::Identifier
     };
 
@@ -32,24 +67,21 @@ pub fn get_token(s : &str) -> Token {
 
 #[inline]
 pub fn has_more_tokens(s : &str) -> bool {
-    return !s.trim().is_empty()
+    return !s.trim().is_empty();
 }
 
-pub fn get_next_token(s : &mut String) -> &str {
+pub fn get_next_token(s : &mut String) -> Option<(String, Token)>
+{
     lazy_static! {
-        static ref RE : Regex = Regex::new(r"\S*\s?").expect("Failed to compile regex");
+        static ref RE : Regex = Regex::new(r"([^\s\W]*[^\W]|[^\w\s])").expect("Failed to compile regex"); //  WHEN U WAKE UP: this works
     }
 
-    if RE.is_match(s) {
+    if RE.is_match(s) 
+    {
+        let found = RE.find(&s).expect("Unable to find match in string").as_str().to_string();
+        let token = get_token(&found);
         *s = RE.replace(s, "").to_string();
-        println!("{}", s);
+        return Some((found, token));
     }
-    return " ";
+    return None;
 }
-
-
-
-
-
-
-
