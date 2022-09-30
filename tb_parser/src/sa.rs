39134c24940329cc0211654fa7f7b66482de212a
ramenu <ramenu101@gmail.com>
2022-09-30 18:48:35 -0400

    add docs

--- tb_parser/src/sa.rs
+++ tb_parser/src/sa.rs
@@ -1,15 +1,7 @@
 
 
-const SA_MODIFIER_LOW : f32 = 1.3;
-const SA_MODIFIER_DAMAGE : f32 = 1.7;
-const SA_MODIFIER_HUGE_DESTRUCTIVE : f32 = 2.0;
-const SA_MODIFIER_EXTREME_MASS : f32 = 2.2;
-const SA_MODIFIER_SUPREME : f32 = 2.5;
-const SA_MODIFIER_IMMENSE : f32 = 2.8;
-const SA_MODIFIER_COLOSSAL : f32 = 3.0;
-const SA_MODIFIER_MEGACOLOSSAL : f32 = 3.5;
 
-pub const MAX_SA_LEVEL : u8 = 25;
+pub const MAX_SA_LEVEL : i32 = 25;
 
 enum Modifier 
 {
@@ -23,22 +15,37 @@ enum Modifier
     MegaColossal
 }
 
-fn get_sa_modifier(s : &str) -> (Modifier, f32)
+/// Returns an optional tuple consisting of the SA modifier
+/// and the damage modifier. If 's' is not an appropriate
+/// super attack modifier word then it will return None.
+fn get_sa_modifier(s : &str) -> Option<(Modifier, f32)>
 {
+    const SA_MODIFIER_LOW : f32 = 1.3;
+    const SA_MODIFIER_DAMAGE : f32 = 1.7;
+    const SA_MODIFIER_HUGE_DESTRUCTIVE : f32 = 2.0;
+    const SA_MODIFIER_EXTREME_MASS : f32 = 2.2;
+    const SA_MODIFIER_SUPREME : f32 = 2.5;
+    const SA_MODIFIER_IMMENSE : f32 = 2.8;
+    const SA_MODIFIER_COLOSSAL : f32 = 3.0;
+    const SA_MODIFIER_MEGACOLOSSAL : f32 = 3.5;
     return match s
     {
-        "low" => (Modifier::Low, SA_MODIFIER_LOW),
-        "damage" => (Modifier::Damage, SA_MODIFIER_DAMAGE),
-        "huge" => (Modifier::HugeDestructive, SA_MODIFIER_HUGE_DESTRUCTIVE),
-        "extreme"|"mass" => (Modifier::ExtremeMass, SA_MODIFIER_EXTREME_MASS),
-        "supreme" => (Modifier::Supreme, SA_MODIFIER_SUPREME),
-        "immense" => (Modifier::Immense, SA_MODIFIER_IMMENSE),
-        "colossal" => (Modifier::Colossal, SA_MODIFIER_COLOSSAL),
-        _ => (Modifier::MegaColossal, SA_MODIFIER_MEGACOLOSSAL)
+        "low" => Some((Modifier::Low, SA_MODIFIER_LOW)),
+        "damage" => Some((Modifier::Damage, SA_MODIFIER_DAMAGE)),
+        "huge" => Some((Modifier::HugeDestructive, SA_MODIFIER_HUGE_DESTRUCTIVE)),
+        "extreme"|"mass" => Some((Modifier::ExtremeMass, SA_MODIFIER_EXTREME_MASS)),
+        "supreme" => Some((Modifier::Supreme, SA_MODIFIER_SUPREME)),
+        "immense" => Some((Modifier::Immense, SA_MODIFIER_IMMENSE)),
+        "colossal" => Some((Modifier::Colossal, SA_MODIFIER_COLOSSAL)),
+        "mega-colossal" => Some((Modifier::MegaColossal, SA_MODIFIER_MEGACOLOSSAL)),
+        _ => None
     };
 }
 
-fn get_sa_atk_stat(atk : f32, modifier_dmg : f32, sa_level : u8) -> f32
+/// Returns the updated attack stat with the super attack buff. This
+/// function should only be called at the very end and if the unit is
+/// performing a super attack. 
+fn get_sa_atk_stat(atk : f32, modifier_dmg : f32, sa_level : i32) -> f32
 {
     debug_assert!(sa_level <= MAX_SA_LEVEL);
     return atk * modifier_dmg * sa_level as f32;
