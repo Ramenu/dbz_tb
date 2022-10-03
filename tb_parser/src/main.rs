9810bad084edddb476cac10a4252a9fbb0ba6a57
ramenu <ramenu101@gmail.com>
2022-10-03 13:27:50 -0400

    test sa retrieval

--- tb_parser/src/main.rs
+++ tb_parser/src/main.rs
@@ -1,11 +1,9 @@
 mod tokenizer;
 mod sa;
+mod tester;
 //mod effectparser;
 fn main() 
 {
-    let mut s = String::from("ATK and DEF +120% at start of turn; Ki +2 in addition per  Ki Sphere obtained; perform Potara fusion when conditions are met");
-    while tokenizer::has_more_tokens(&s) {
-        let tokens = tokenizer::get_n_tokens(&mut s, 4, true).expect("Not enough tokens");
-        println!("{}", tokens.0);
-    }
+    let s = "and causes immense damage to enemy".to_string();
+    tester::test_sa_retrieval();
 }
