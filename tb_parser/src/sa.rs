a335133d8f4ae901847f2ef343568125d962ee74
ramenu <ramenu101@gmail.com>
2022-10-04 12:55:49 -0400

    add sainfo impl, and more sa fns

--- tb_parser/src/sa.rs
+++ tb_parser/src/sa.rs
@@ -2,9 +2,17 @@ use wasm_bindgen::prelude::*;
 use regex::Regex;
 use lazy_static::lazy_static;
 use  std::fmt;
-use crate::tokenizer;
+
+use crate::{tokenizer, effectparser::{self, EFF_RAISES, EFF_GREATLY, EFF_LOWERS, EFF_ALL, EFF_ENEMY, EFF_ATK, EFF_DEF, NULL}, modifier};
 
 pub const MAX_SA_LEVEL : i32 = 25;
+
+pub const SAEFFECT_NULL : u32 = 0x0;
+pub const SAEFFECT_STUN : u32 = 0x1;
+pub const SAEFFECT_SEAL : u32 = 0x2;
+pub const SAEFFECT_ATK_ALL_ENEMIES : u32 = 0x4;
+pub const SAEFFECT_STATCHANGE_ON_ENEMY : u32 = 0x8;
+pub const SAEFFECT_STATCHANGE_ON_ALL_ENEMIES : u32 = 0x10;
 
 lazy_static! {
     static ref SA_RE : Regex = Regex::new(r"^causes (low damage|damage|huge damage|extreme damage|mass damage|supreme damage|immense damage|colossal damage|mega-colossal damage)(?: to (?:the|all)? ?(?:enemy|enemies))?")
@@ -18,7 +26,7 @@ lazy_static! {
 pub struct SaInfo
 {
     modifier : Modifier,
-    effect : SaEffect,
+    effect : u32,
     atk_buff : f32,
     def_buff : f32,
     atkdef_buff_turn_count : u16, // Includes attack/def buff count in same variable
@@ -27,6 +35,39 @@ pub struct SaInfo
     enemy_def_reduction : f32,
     enemy_atk_reduction_turn_count : u32,
     enemy_def_reduction_turn_count : u32
+}
+
+#[wasm_bindgen]
+impl SaInfo
+{
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> SaInfo {
+        return SaInfo { modifier: Modifier::Low, 
+                        effect: SAEFFECT_NULL, 
+                        atk_buff: 0.0, def_buff: 0.0, 
+                        atkdef_buff_turn_count: 0, 
+                        turns_to_stunseal: 0, 
+                        enemy_atk_reduction: 0.0, 
+                        enemy_def_reduction: 0.0, 
+                        enemy_atk_reduction_turn_count: 0, 
+                        enemy_def_reduction_turn_count: 0 };
+    }
+    #[wasm_bindgen(getter = modifier)]
+    pub fn get_modifier(&self) -> Modifier {
+        return self.modifier;
+    }
+    #[wasm_bindgen(getter = effect)]
+    pub fn get_effect(&self) -> u32 {
+        return self.effect;
+    }
+    #[wasm_bindgen(getter = atk_buff)]
+    pub fn get_atk_buff(&self) -> f32 {
+        return self.atk_buff;
+    }
+    #[wasm_bindgen(getter = def_buff)]
+    pub fn get_def_buff(&self) -> f32 {
+        return self.def_buff;
+    }
 }
 
 #[wasm_bindgen]
@@ -62,14 +103,7 @@ impl fmt::Display for Modifier
     }
 }
 
-#[derive(Clone, Copy, Default)]
-pub enum SaEffect
-{
-    #[default] Null = 0x0,
-    Stun = 0x1,
-    Seal = 0x2,
-    AtkAllEnemies = 0x4
-}
+
 
 #[inline]
 pub fn get_sa_match(s : &str) -> Option<regex::Match> {
@@ -123,7 +157,7 @@ pub fn get_sa_modifier_atk(modifier : Modifier) -> f32
         Modifier::Immense => (SA_MODIFIER_IMMENSE),
         Modifier::Colossal => (SA_MODIFIER_COLOSSAL),
         Modifier::MegaColossal => (SA_MODIFIER_MEGACOLOSSAL)
-    };
+    } + 1.0;
 }
 
 /// Returns the updated attack stat with the super attack buff. This
@@ -133,6 +167,34 @@ pub fn get_sa_atk_stat(atk : f32, modifier_dmg : f32, sa_level : i32) -> f32
 {
     assert!(sa_level <= MAX_SA_LEVEL);
     return atk * modifier_dmg * sa_level as f32;
+}
+
+/// Modifies the sa struct with the proper changes to be made depending
+/// on the stat effect.
+fn get_sa_stat_change_eff(eff : effectparser::StatEffect, sa : &mut SaInfo)
+{
+    let mut boost = 0.0f32;
+    if eff.get_stat_effect()&EFF_GREATLY != NULL {
+        boost += modifier::EFF_GREATLY_INC_OR_DEC_MODIFIER;
+    }
+    if eff.get_stat_effect()&EFF_RAISES != NULL {
+        boost += modifier::EFF_INC_OR_DEC_MODIFIER;
+    }
+    if eff.get_stat_effect()&EFF_LOWERS != NULL {
+        boost -= modifier::EFF_INC_OR_DEC_MODIFIER;
+    }
+    if eff.get_stat_effect()&EFF_ALL != NULL {
+        sa.effect |= SAEFFECT_ATK_ALL_ENEMIES;
+    }
+    if eff.get_stat_effect()&EFF_ENEMY != NULL {
+        sa.effect |= SAEFFECT_STATCHANGE_ON_ENEMY;
+    }
+    if eff.get_stat_effect()&EFF_ATK != NULL {
+        sa.atk_buff += boost;
+    }
+    if eff.get_stat_effect()&EFF_DEF != NULL {
+        sa.def_buff += boost;
+    }
 }
 
 /// Call this function with the super attack lowercased!
@@ -151,12 +213,19 @@ pub fn parse_super_attack(sa_eff : &str) -> SaInfo
     while tokenizer::has_more_tokens(&s)
     {
         let token = tokenizer::get_next_token(&mut s, false).expect("Could not retrieve next token");
-        if tokenizer::is_skippable_token(&token) {
+        if tokenizer::is_skippable_token(&token) 
+        {
             tokenizer::skip_token(&mut s);
             continue;
         }
-        if token.1 == tokenizer::Token::Keyword {
-
+        if token.1 == tokenizer::Token::Keyword 
+        {
+            // Check if it raises or lowers atk/def of enemy or self
+            let eff_opt = effectparser::raises_or_lowers_stat(&mut s, true);
+            if eff_opt.is_some() {
+                let eff = eff_opt.expect("Failed to retrieve stat effect");
+                get_sa_stat_change_eff(eff, &mut sa);
+            }
         }
     }
     return sa;
