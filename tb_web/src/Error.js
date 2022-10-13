411896686abc574cc1f5d32b0f76ffc83a27b3a2
ramenu <ramenu101@gmail.com>
2022-10-13 19:11:16 -0400

    minor error msg changes

--- tb_web/src/Error.js
+++ tb_web/src/Error.js
@@ -8,4 +8,4 @@ export const invalidURLMessage = (s) => `Invalid URL: ${s}`;
 
 export const invalidRarityMessage = (s) => `Invalid rarity: ${s}\nPossible rarities are: [N, R, SR, SSR, UR, LR]`;
 
-export const invalidTypeMessage = (s) => `Invalid type: ${s}\nPossible types are: [STR, AGL, TEQ, PHY, INT]`;
+export const invalidTypeMessage = (s) => `Invalid type: ${s}\nPossible types are: \"Super|Extreme [STR, AGL, TEQ, PHY, INT]\"`;
