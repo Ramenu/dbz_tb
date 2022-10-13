82c8f1d6a3224d4e08aa806a8fef2b8579604cb4
ramenu <ramenu101@gmail.com>
2022-10-13 19:11:32 -0400

    add in missing fields

--- tb_web/src/Unit.js
+++ tb_web/src/Unit.js
@@ -1,8 +1,9 @@
-import { isValidRarity, isValidType, isValidURL } from "./Checker";
-import { invalidRarityMessage, invalidTypeMessage, invalidURLMessage } from "./Error";
+'use strict';
+import { isValidRarity, isValidType, isValidURL } from "./Checker.js";
+import { invalidRarityMessage, invalidTypeMessage, invalidURLMessage } from "./Error.js";
 
 
-export class Unit
+export default class Unit
 {
     /**
      * 
@@ -15,6 +16,7 @@ export class Unit
      * @param {String} leaderSkill 
      * @param {String} passiveSkill 
      * @param {String} superAtk 
+     * @param {String} awakensInto
      * @param {String[]} categories 
      * @param {Number} atk 
      * @param {Number} def 
@@ -23,8 +25,9 @@ export class Unit
     constructor(url, icon, art,
                 name, rarity, type,
                 leaderSkill, passiveSkill,
-                superAtk, categories,
-                atk, def, hp) {
+                activeSkill, superAtk, 
+                ultraSa, unitSa, unitSaCondition,
+                awakensInto, categories, atk, def, hp) {
         
         if (!isValidURL(url))
             throw invalidURLMessage(url);
@@ -32,10 +35,14 @@ export class Unit
             throw invalidURLMessage(icon);
         if (!isValidURL(art))
             throw invalidURLMessage(art);
+        if (awakensInto !== undefined) // This is allowed, as all objects may not have a dokkan awakening
+            if (!isValidURL(awakensInto))
+                throw invalidURLMessage(awakensInto);
         if (!isValidRarity(rarity))
             throw invalidRarityMessage(rarity);
-        if (!isValidType(type))
-            throw invalidTypeMessage(type);
+        if (type !== undefined) // Some units (like selling statues) may not have a SUPER/EXTREME type
+            if (!isValidType(type))
+                throw invalidTypeMessage(type);
         if (!Number.isInteger(atk))
             throw `Invalid ATK stat: ${atk}`;
         if (!Number.isInteger(def))
@@ -52,7 +59,12 @@ export class Unit
         this.type = type;
         this.leaderSkill = leaderSkill;
         this.passiveSkill = passiveSkill;
+        this.activeSkill = activeSkill;
         this.superAtk = superAtk;
+        this.ultraSa = ultraSa;
+        this.unitSa = unitSa;
+        this.unitSaCondition = unitSaCondition;
+        this.awakensInto = awakensInto;
         this.categories = categories;
         this.atk = atk;
         this.def = def;
