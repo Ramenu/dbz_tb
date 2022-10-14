1e5b0009a0dba9926ddd82bc36b6022014bcb7fc
ramenu <ramenu101@gmail.com>
2022-10-13 22:01:01 -0400

    remove wee_alloc allocator

--- tb_parser/src/lib.rs
+++ tb_parser/src/lib.rs
@@ -6,11 +6,13 @@ mod effect;
 
 use wasm_bindgen::prelude::*;
 
+/*
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+*/
 
 #[wasm_bindgen]
 extern {
