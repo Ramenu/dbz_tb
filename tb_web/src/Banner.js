f4267a130544515eeb98b2c08977b26523acf2b6
ramenu <ramenu101@gmail.com>
2022-10-14 20:44:45 -0400

    remove getters and add ALL_BANNERS

--- tb_web/src/Banner.js
+++ tb_web/src/Banner.js
@@ -13,16 +13,17 @@ export class Banner
         this.featuredUnits = featuredUnits;
         this.image = bannerImage;
     }
-
-    get type() {
-        return this.type;
-    }
-
-    get featuredUnits() {
-        return this.featuredUnits;
-    }
-
-    get image() {
-        return this.image;
-    }
 }
+
+
+export const ALL_BANNERS = [
+    new Banner(
+        BANNER_TYPE.Null,
+        [
+            "https://dbz-dokkanbattle.fandom.com/wiki/The_Saiyan_Among_Us_Goku",
+            "https://dbz-dokkanbattle.fandom.com/wiki/Genius_of_War_Vegeta",
+            "https://dbz-dokkanbattle.fandom.com/wiki/Stern_Teacher_Piccolo"
+        ],
+        "https://static.wikia.nocookie.net/dbz-dokkanbattle/images/c/c6/Banner_dokkan_1.jpg/revision/latest?cb=20180312125355"
+    )
+];
