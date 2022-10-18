2fed2e409f825efc34fcc19483602a1032c41b5d
ramenu <ramenu101@gmail.com>
2022-10-18 18:28:16 -0400

    add depStones and withdrawStones fn to reducer

--- tb_web/src/redux/reducers/UserReducer.tsx
+++ tb_web/src/redux/reducers/UserReducer.tsx
@@ -1,19 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { User } from "../../jsdependencies/User";
 
 const initialData = () : User => {
     return new User();
 }
 
-/*
+
 export const userReducer = createSlice({
     name: "user",
     initialState: {
         info: initialData()
     },
     reducers: {
-        depositStones: (state, action) => {
+        depositStones: (state, action : PayloadAction<number>) => {
+            state.info.ds += action.payload;
+        },
+        withdrawStones: (state, action : PayloadAction<number>) => {
+            state.info.ds += action.payload;
         }
 
     }
-});*/
+});
