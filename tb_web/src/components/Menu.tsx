b4ea20e54074a9696b437b44ccfd970c02d3d5ef
ramenu <ramenu101@gmail.com>
2022-10-19 21:24:47 -0400

    add zeni/ds to menu

--- tb_web/src/components/Menu.tsx
+++ tb_web/src/components/Menu.tsx
@@ -4,26 +4,33 @@ import buttonStyles from "../styles/button.module.scss";
 import topMenuStyles from "../styles/topmenu.module.scss";
 import { useDispatch, useSelector } from "react-redux";
 import dsLogo from "../assets/images/dragon_stone.png";
+import zeniLogo from "../assets/images/zeni.png";
+import { User } from "../tsdependencies/User";
 
 
 export const TopMenu = () => 
 {
-    let rank : number = useSelector((state : any) => state.user.info.rank);
-    let ds : number = useSelector((state : any) => state.user.info.ds);
-    let divText : JSX.Element = <div className={topMenuStyles.rankText}>{rank}</div>;
-    if (rank >= 0 && rank <= 9)
-        divText = <div className={topMenuStyles.rankText}>&nbsp;{rank}</div>;
-    else if (rank >= 100)
+    const user : User = useSelector((state : any) => state.user.info);
+    let divText : JSX.Element = <div className={topMenuStyles.rankText}>{user.rank}</div>;
+    if (user.rank >= 0 && user.rank <= 9)
         divText = (<div className={topMenuStyles.rankText}>
-                        <div className={topMenuStyles.rankTextSmall}>{rank}</div>
+                    <div className={topMenuStyles.alignToLeft}>&nbsp;{user.rank}</div>
+                  </div>);
+    else if (user.rank >= 100)
+        divText = (<div className={topMenuStyles.rankText}>
+                        <div className={topMenuStyles.small}>{user.rank}</div>
                    </div>);
     // TODO: Add functionality to click top menu to bring down contents
     return (
         <div>
             <div className={topMenuStyles.menuBar}>
+                <div className={topMenuStyles.zeniDisplay}>
+                    <img src={zeniLogo} alt="Zeni"/>
+                    {user.zeni}
+                </div>
                 <div className={topMenuStyles.stoneDisplay}>
-                    <img src={dsLogo}/>
-                    500000
+                    <img src={dsLogo} alt="Dragon Stones"/>
+                    {user.ds}
                 </div>
             </div>
             <div className={topMenuStyles.rankCircle}>
