f37f2f053f5c14ef12c812bb3c38cc2ed2498dc1
ramenu <ramenu101@gmail.com>
2022-10-23 22:36:14 -0400

    add imgSources.tsx

---
+++ tb_web/src/tsdependencies/ImgSources.tsx
@@ -0,0 +1,3 @@
+
+export const DS_ICON : string = "https://static.wikia.nocookie.net/dbz-dokkanbattle/images/2/27/DS_icon.png";
+export const ZENI_ICON : string = "https://static.wikia.nocookie.net/dbz-dokkanbattle/images/3/39/Zeni.png";
