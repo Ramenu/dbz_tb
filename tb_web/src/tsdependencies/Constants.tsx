c871780ad545ac4a26a69d0d8bcd31067c82af8d
ramenu <ramenu101@gmail.com>
2022-10-24 15:17:35 -0400

    add constants.tsx

---
+++ tb_web/src/tsdependencies/Constants.tsx
@@ -0,0 +1,5 @@
+
+export const ACTION_MULTI_SUMMON : string = "ACTION_MULTI_SUMMON";
+export const STONES_TO_SINGLE_SUMMON : number = 5;
+export const STONES_TO_MULTI_SUMMON : number = 50;
+export const MULTI_SUMMON_ROLL_COUNT : number = 10;
