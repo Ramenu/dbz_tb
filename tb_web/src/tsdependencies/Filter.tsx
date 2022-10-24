ba83628e61784084ddb6bb2fb6b9bb288e40658e
ramenu <ramenu101@gmail.com>
2022-10-24 15:18:37 -0400

    shorten filterUnits fn

--- tb_web/src/tsdependencies/Filter.tsx
+++ tb_web/src/tsdependencies/Filter.tsx
@@ -1,37 +1,33 @@
-import { isValidRarity, isValidType } from "./Checker.js"
-import { invalidRarityMessage, invalidTypeMessage } from "./Error.js"
-import Unit from "./Unit.js";
+import { isValidRarity, isValidType } from "./Checker"
+import { invalidRarityMessage, invalidTypeMessage } from "./Error"
+import Unit from "./Unit";
 
 export enum FilterAction
 {
-    Rarity,
-    Type
+    Rarity = "RARITY",
+    Type = "TYPE"
 }
 
 export const filterUnits = (units : Unit[], filter : string, action : FilterAction) : Unit[] => {
-    if (action == FilterAction.Rarity)
+    let filteredUnits : Unit[] = [];
+    if (action === FilterAction.Rarity)
     {
         if (!isValidRarity(filter))
             throw invalidRarityMessage(filter);
+        units.forEach((unit) => {
+            if (unit.rarity === filter)
+                filteredUnits.push(unit);
+        });
     }
     else
     {
         if (!isValidType(filter))
             throw invalidTypeMessage(filter);
+        units.forEach((unit) => {
+            if (unit.type === filter)
+                filteredUnits.push(unit);
+        });
     }
     
-    let filteredUnits : Unit[] = [];
-    units.forEach(unit => {
-        if (action == FilterAction.Rarity)
-        {
-            if (unit.rarity == filter)
-                filteredUnits.push(unit);
-        }
-        else
-        {
-            if (unit.type == filter)
-                filteredUnits.push(unit);
-        }
-    });
     return filteredUnits;
 }
