983b91f6f47e0dde70026aca94490c9db78886ed
ramenu <ramenu101@gmail.com>
2022-10-31 14:38:58 -0400

    add traits.rs

---
+++ tb_parser/src/traits.rs
@@ -0,0 +1,16 @@
+use std::str::FromStr;
+
+
+pub trait Number {}
+
+impl Number for i8 {}
+impl Number for u8 {}
+impl Number for i16 {}
+impl Number for u16 {}
+impl Number for i32 {}
+impl Number for u32 {}
+impl Number for i64 {}
+impl Number for u64 {}
+impl Number for usize {}
+impl Number for f32 {}
+impl Number for f64 {}
